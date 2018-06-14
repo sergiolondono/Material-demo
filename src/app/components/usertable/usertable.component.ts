c3394d540af8b71a756a309f8b312d0fbe87fd84
sergiolondono <sergio.londonog@gmail.com>
2018-06-14 16:40:25 -0500

    20180614

--- src/app/components/usertable/usertable.component.ts
+++ src/app/components/usertable/usertable.component.ts
@@ -1,32 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
-import {DataSource} from '@angular/cdk/collections';
+import {DataSource, CollectionViewer} from '@angular/cdk/collections';
 import { User } from '../../models/user.model';
+import { MatPaginator, MatSort } from '@angular/material';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { of } from 'rxjs/observable/of';
+import { catchError, finalize, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'usertable',
   templateUrl: './usertable.component.html',
   styleUrls: ['./usertable.component.css']
 })
+
 export class UsertableComponent implements OnInit {
 
   dataSource = new UserDataSource(this.userService);
   displayedColumns = ['name', 'email', 'phone', 'company'];
+  usersService;
+
   constructor(private userService: UserService) { }
 
-  ngOnInit() {
+   ngOnInit() {
   }
-
 }
 
 export class UserDataSource extends DataSource<any> {
   constructor(private userService: UserService) {
     super();
   }
+
   connect(): Observable<User[]> {
     return this.userService.getUser();
   }
+
   disconnect() {}
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable }   from '@angular/core';
-import { HttpClient }   from '@angular/common/http';
+import { HttpClient, HttpParams }   from '@angular/common/http';
 import { Observable }   from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { User } from '../models/user.model';
+import { map } from 'rxjs/operators/map';
 
 @Injectable()
 export class UserService {
@@ -15,4 +16,18 @@ export class UserService {
     return this.http.get<User[]>(this.serviceUrl);
   }
   
+  findUsers(filter = '', sortOrder = 'asc',
+    pageNumber = 0, pageSize = 3): Observable<User[]> {
+
+    return this.http.get(this.serviceUrl, {
+        params: new HttpParams()
+            //.set('id', id.toString())
+            .set('filter', filter)
+            .set('sortOrder', sortOrder)
+            .set('pageNumber', pageNumber.toString())
+            .set('pageSize', pageSize.toString())
+    }).pipe(
+        map(res => res["payload"])
+    );
+}
 }
